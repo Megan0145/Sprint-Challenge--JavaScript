3b7f3c7ddddd6355065b989e08d2b1a00df15cde
Megan <mennis0145@gmail.com>
2019-08-23 08:23:39 +0100

    Object qs completed

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -8,28 +8,54 @@
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
 
+let tyrannosaurus = {
+  name: 'Tyrannosaurus',
+  diet: 'Carnivorous',
+  weight: 700 + 'kg',
+  length: 12 + 'm',
+  period: 'Late Cretaceous',
+  roar: function() {
+    return 'RAWERSRARARWERSARARARRRR!';
+  },
+}
+
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 
+let stegosaurus = {
+  name: 'Stegosaurus',
+  diet: 'Herbivorous',
+  weight: 2000 + 'kg',
+  length: 9 + 'm',
+  period: 'Late Jurassic',
+}
+
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+
+let velociraptor = {
+  name: 'Velociraptor',
+  diet: 'Carnivorous',
+  weight: 15 + 'kg',
+  length: 1.8 + 'm',
+  period: 'Late Cretaceous',
+}
 
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tyrannosaurus.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velociraptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegosaurus.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
-
+console.log(tyrannosaurus.roar());
 
 // ==== Arrays ====
 
